97c69a30e7f882ccef22092ddcf9c9c7f4a8b697
Alessandra Lima <56393487+alewssandra@users.noreply.github.com>
2021-06-25 19:42:24 -0300

    Add files via upload

---
+++ n3/Retangulo.java
@@ -0,0 +1,31 @@
+package n3;
+
+public class Retangulo {
+
+    //Atributos
+    public float altura, largura;
+
+    //Construtor
+    public Retangulo(float altura, float largura) {
+        this.altura = altura;
+        this.largura = largura;
+    }
+    
+    //Métodos
+    public float calcularArea(){
+        float area = altura * largura;
+        return area;
+    }
+    
+    public float calcularPerimetro(){
+        float perimetro = (2*altura + 2*largura);
+        return perimetro;
+    }
+    
+    public void imprimir(){
+        System.out.println("Altura: " + this.altura);
+        System.out.println("Largura: " + this.largura);
+        System.out.println("Área: " + calcularArea());
+        System.out.println("Perímetro: " + calcularPerimetro() + "\n");
+    }
+}
