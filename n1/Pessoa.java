18288d7d127c29938aaf87bc71e9c9e171ba04d9
Alessandra Lima <56393487+alewssandra@users.noreply.github.com>
2021-06-25 19:39:15 -0300

    Add files via upload

---
+++ n1/Pessoa.java
@@ -0,0 +1,33 @@
+package n1;
+
+public class Pessoa {
+    //Atributos
+    public String nome;
+    public int peso, idade;
+
+    //Construtor
+    public Pessoa(String nome, int peso, int idade) {
+        this.nome = nome;
+        this.peso = peso;
+        this.idade = idade;
+    }
+    
+    //Métodos
+    public void caminha(){
+        this.peso--;
+    }
+    
+    public void come(){
+        this.peso++;
+    }
+    
+    public void fazAniversario(){
+        this.idade++;
+    }
+    
+    public void imprime(){
+        System.out.println("Nome: " + this.nome);
+        System.out.println("Idade: " + this.idade);
+        System.out.println("Peso: " + this.peso + "\n");
+    }    
+}
